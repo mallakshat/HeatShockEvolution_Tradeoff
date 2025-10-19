d7cfee71852c0f20a7cd90a380094637ecac6491
mallakshat <70936499+mallakshat@users.noreply.github.com>
2025-10-19 15:36:54 -0700

    Create FigS2.R

---
+++ Supplementary_Code/FigS2.R
@@ -0,0 +1,35 @@
+#S2 - evolved vs ancestor in evolution regime
+data = read.csv("CsvFilesForFigures/Evolved_GrowthCurve_HS.csv")
+metadata = read.csv("CsvFilesForFigures/Evolved_GrowthCurve_HS_metadata.csv")
+
+data$Time = data$Time/60
+data_long <- data %>%
+  pivot_longer(cols = -Time, names_to = "Well", values_to = "OD") %>%
+  left_join(metadata, by = "Well")
+
+blank <- data_long %>%
+  filter(Isolate == "Blank") %>%
+  summarize(blank = mean(OD, na.rm = TRUE)) %>%
+  pull(blank)
+
+data_long <- data_long %>%
+  mutate(OD = OD - blank)
+
+data_long <- data_long %>%
+  filter(Isolate != "Blank")
+
+plot_concentration <- function(env) {
+  ggplot() +
+    geom_point(data = data_long %>% filter(Isolate == "Anc"),aes(x = Time, y = OD, group = Well),color = "black", alpha = 0.5,size=2) +
+    geom_point(data = data_long %>% filter(Isolate == "Anc"),aes(x = Time, y = OD, group = Well),color = "darkgray", alpha = 0.5,size=1) +
+    geom_point(data = data_long %>% filter(Isolate == env),aes(x = Time, y = OD, group = Well),color="black",size=2) +
+    geom_point(data = data_long %>% filter(Isolate == env),aes(x = Time, y = OD, color = Strain, group = Well),color="deepskyblue",size=1) +
+    #scale_color_brewer(name = "Isolate", palette = "Set1")+
+    theme_classic() + theme(text = element_text(size = 30))+
+    ggtitle(env)+xlab(NULL)+ylab(NULL)+ylim(0,0.18)+xlim(0,35)
+    #guides(color = guide_legend(override.aes = list(size = 7)))+xlim(0,40) 
+}
+
+tiff("Supplementary/HS growth curves/J1_HS.tiff", width = 5, height = 5, units = "in", res = 300)
+plot_concentration("J1")
+dev.off()
